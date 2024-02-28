6a7633ceeb86245154bdca3917745b8aae3d0bce
nahoslevvy12@gmail.com <nahoslevvy12@gmail.com>
2024-02-28 17:39:10 +0000

    first commit 28/02/2024

---
+++ public/data.js
@@ -0,0 +1,39 @@
+export let pokemons = [
+    {
+        id:1,
+        name:"pokemon 1",
+        type: [
+            'pilonce',
+            'poison'
+        ],
+        create_at:new Date()
+    },
+    {
+        id:2,
+        name:"pokemon 2",
+        type: [
+            'jouer',
+            'foroce'
+        ],
+        create_at:new Date()
+    },
+    {
+        id:3,
+        name:"pokemon 3",
+        type: [
+            'lonce',
+            'jorne'
+        ],
+        create_at:new Date()
+    },
+    {
+        id:4,
+        name:"pokemon 4",
+        type: [
+            'pilonce',
+            'poison'
+        ],
+        create_at:new Date()
+    }
+
+]
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-const express = require('express')
+import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
+import {pokemons} from '../public/data.js'
+import {giveId, response} from './helpers.js'
 const app = express()
+const port = 3000
+
+app
+.use(morgan('dev'))
+.use(bodyParser.json())
+
 
 app.get("/",(req,res)=>{
-   res.json({
-    message:"Hello express JS"
-   })
+   res.send("Hello Express JS")
 })
-app.listen(4000,(error)=>{
+app.get("/api/pokemons/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon=>pokemon.id === id)
+    if(pokemon == undefined){
+        throw new Error("Pokemon non trouvé")
+    }
+    res.json(response("pokémon trouvé",pokemon))
+})
+app.get("/api/pokemons",(req,res)=>{
+   res.json(response("La liste des pokémon a bien été retournée",pokemons))
+})
+app.post('/api/pokemons',(req,res)=>{
+    const id = giveId(pokemons)
+    console.log(req.body)
+    const pokemon = {... req.body, ...{id:id,created_at: new Date()}}
+    pokemons.push(pokemon)
+    res.json(response("User crée avec success",pokemon))
+})
+
+app.put('/api/pokemons/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemonUpdate = {... req.body,id:id}
+
+    pokemons = pokemons.map((pokemon)=>{
+        return pokemon.id === id ? pokemonUpdate:pokemon
+    })
+
+    res.json(response("pokemon updated!!",pokemonUpdate))
+})
+
+app.listen(port,(error)=>{
     if(error){
         throw error
     }
-    console.log("Le serveur écoute sur le port 4000")
+    console.log(`Le serveur écoute sur http://localhost:${port}`)
 })
