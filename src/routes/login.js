1a4250e9b9135ee0894546dd0339dcf34e8a83d8
nahoslevvy12@gmail.com <nahoslevvy12@gmail.com>
2024-03-04 10:55:08 +0000

    second commit 04/03/2024

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt'
 import { userModel } from '../db/sequelise.js'
+import { TokenSigner } from'jsontokens'
+import { privateKey } from '../auth/private_key.js'
 export function login(app){
 
     app.post('/api/login',(req,res)=>{
@@ -15,8 +17,11 @@ export function login(app){
                 const msg = "Le mot de passe est incorrect!!"
                 res.status(401).json({msg})
             }
+            //const rawPrivateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
+            const tokenPayload = {"iat": 1440713414.85,"user_id":userExist.id}
+            const token = new TokenSigner('ES256K', privateKey).sign(tokenPayload)
             const msg = "Cool!!"
-            res.status(201).json({msg})
+            res.status(201).json({msg,data:token})
         })
        }).catch(error=>{
         const msg = "Utilisateur non trouvé!!"
